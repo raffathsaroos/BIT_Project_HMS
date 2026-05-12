d6e526a80880e76ed328f4a5cfabd3ab31c5e88e
raffathsaroos <raffaq8sarose@gmail.com>
2026-05-12 19:39:29 +0530

    simple login page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,6 @@
-import { Routes, Route, Navigate } from "react-router-dom";
+/*import { Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/Login";
-import Signup from "./pages/Signup";
-import Dashboard from "./pages/Dashboard";
-import ProtectedRoute from "./components/ProtectedRoute";
+
 
 function App() {
   return (
@@ -10,12 +8,39 @@ function App() {
       <Route path="/" element={<Navigate to="/login" replace />} />
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
-      <Route path="/dashboard" element={
+      <Route path="/home" element={
         <ProtectedRoute>
-          <Dashboard />
+          <Home />
         </ProtectedRoute>
       } />
     </Routes>
+	
+	<div>
+	
+	<Login />
+	
+	</div>
+	
+  );
+}
+
+export default App;*/
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Home from "./pages/Home"; // Home-ஐயும் Import செய்ய வேண்டும்
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+	    {/* ஆரம்பத்தில் லாகின் பக்கம் தெரிய path="/" என வைக்கலாம் */}
+        <Route path="/" element={<Login />} />
+        
+        {/* ஹோம் பக்கத்திற்கான பாதை */}
+        <Route path="/home" element={<Home />} />
+      </Routes>
+    </Router>
   );
 }
 
